61ceec562261d15bfa15b7e21674e9c83f323f16
Ali Rough <ali.hastam2015@gmail.com>
2023-02-26 19:42:01 +0330

    add rotation

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const panoImg = "../../image/panaroma.jpg";
 const panorama =new PANOLENS.ImagePanorama("../../image/panaroma.jpg");
 const viewer = new PANOLENS.Viewer({
   container: panoImgElem,
+  autoRotate:true,
+  autoRotateSpeed: 0.5
+
 });
 viewer.add(panorama);
 
